b626d8737869d949192d1d7c212feecb0a974da8
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-18 12:11:55 +0700

    auth login

--- controllers/login_controller.go
+++ controllers/login_controller.go
@@ -1,1 +1,52 @@
 package controllers
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/teten-nugraha/simple-go-rest/helpers"
+	"github.com/teten-nugraha/simple-go-rest/models"
+	"net/http"
+	"time"
+)
+
+func GenerateHashPassword(c echo.Context) error {
+	password := c.Param("password")
+
+	hash, _ := helpers.HashPassword(password)
+
+	return c.JSON(http.StatusOK, hash)
+}
+
+func CheckLogin(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	res, err := models.CheckLogin(username, password)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"messages": err.Error(),
+		})
+	}
+
+	if !res {
+		return echo.ErrUnauthorized
+	}
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["level"] = "application"
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"messages": err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token":t,
+	})
+}
--- models/login_model.go
+++ models/login_model.go
@@ -1,1 +1,44 @@
 package models
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/teten-nugraha/simple-go-rest/db"
+	"github.com/teten-nugraha/simple-go-rest/helpers"
+)
+
+type User struct {
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+func CheckLogin(username, password string) (bool, error) {
+
+	var obj User
+	var pwd string
+
+	con := db.CreateConf()
+
+	sqlStatement := "SELECT * FROM users WHERE username = ? "
+	err := con.QueryRow(sqlStatement, username).Scan(
+		&obj.Id, &obj.Username, &pwd,
+		)
+
+	if err == sql.ErrNoRows {
+		fmt.Println("Username not found")
+		return false, err
+	}
+
+	if err != nil {
+		fmt.Println("Query error")
+		return false, err
+	}
+
+	match, err := helpers.CheckPasswordHash(password, pwd)
+	if !match {
+		fmt.Print("Hash and password doesnt match")
+		return false, err
+	}
+
+	return true, nil
+}
